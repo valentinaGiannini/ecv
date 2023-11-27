3bd8170d5e4a6a009e2681ca1f5d82664eaec34a
Valentina Giannini <47178936+valentinaGiannini@users.noreply.github.com>
2023-11-27 12:33:53 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -2,5 +2,30 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Calcoliamo ecv')
 
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+basale_path = 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/basale.nii'
+le_path = 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/LE.nii'
+
+
+# Read the .nii image c
+basale = sitk.ReadImage(basale_path)
+lee = sitk.ReadImage(le_path)
+ecv_array = 0.6 * (sitk.GetArrayFromImage(lee) - sitk.GetArrayFromImage(basale))/198.0
+ecv_array[ecv_array < 0.0] = 0.0
+ecv_array[ecv_array > 8.0] = 8.0
+ecv_array_int=np.round(ecv_array*10)
+
+ecv=sitk.GetImageFromArray(ecv_array_int)
+castFilter = sitk.CastImageFilter()
+castFilter.SetOutputPixelType(sitk.sitkInt16)
+
+# Convert floating type image (imgSmooth) to int type (imgFiltered)
+imgFiltered = castFilter.Execute(ecv)
+
+for i in range(imgFiltered.GetDepth()):
+    image_slice = imgFiltered[:, :, i]
+    sitk.WriteImage(image_slice, 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/dcm80/'+str(i)+'.dcm')
