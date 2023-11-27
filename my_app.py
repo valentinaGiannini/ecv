111c8799d9086606f06c40fcd8823fcb2126ca49
Valentina Giannini <47178936+valentinaGiannini@users.noreply.github.com>
2023-11-27 14:39:56 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -10,8 +10,7 @@ import numpy as np
 # folder_path='C:Users/Valentina/Downloads/'
 # filenames = os.listdir(folder_path)
 # selected_filename = st.selectbox('Select a file', filenames)
-# #return os.path.join(folder_path, selected_filename)
-    
+# #return os.path.join(folder_path, selected_filename)   
 # filename = file_selector()   
 st.title('Calcoliamo ecv')
 
