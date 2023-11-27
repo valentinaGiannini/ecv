dd6b311efa91870436557448ef3d7e73ca87be2e
Valentina Giannini <47178936+valentinaGiannini@users.noreply.github.com>
2023-11-27 13:59:29 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -12,7 +12,7 @@ def file_selector(folder_path='C:Users/Valentina/Downloads/'):
     selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
     
-filename = file_selector()    
+# filename = file_selector()    
 st.title('Calcoliamo ecv')
 
 HT = st.number_input('Imposta valore Ematocrito')
