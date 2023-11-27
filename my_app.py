2036492bc0a28e12e643f8519a510677f14bcfdf
Valentina Giannini <47178936+valentinaGiannini@users.noreply.github.com>
2023-11-27 13:13:55 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -11,16 +11,19 @@ st.title('Calcoliamo ecv')
 HT = st.number_input('Imposta valore Ematocrito')
 risultato = 1- HT
 # st.write(1-HT)
-st.write(f' {risultato}!')
+# st.write(f' {risultato}')
+
+deltaBlood = st.number_input('Imposta deltablood')
 
 basale_path = 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/basale.nii'
 le_path = 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/LE.nii'
 
 
+
 # # Read the .nii image c
 # basale = sitk.ReadImage(basale_path)
 # lee = sitk.ReadImage(le_path)
-# ecv_array = (1-HT) * (sitk.GetArrayFromImage(lee) - sitk.GetArrayFromImage(basale))/198.0
+# ecv_array = (1-HT) * (sitk.GetArrayFromImage(lee) - sitk.GetArrayFromImage(basale))/deltaBlood
 # ecv_array[ecv_array < 0.0] = 0.0
 # ecv_array[ecv_array > 8.0] = 8.0
 # ecv_array_int=np.round(ecv_array*10)
