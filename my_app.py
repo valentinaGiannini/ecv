d3780e5b278bc2f0a0bab832bcc4e93c45216646
Valentina Giannini <47178936+valentinaGiannini@users.noreply.github.com>
2023-11-27 13:17:42 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,7 +1,8 @@
+pip install -r requirements.txt
 import streamlit as st
 import pandas as pd
 import numpy as np
-# pip install -r requirements.txt
+# 
 # import SimpleITK as sitk
 import numpy as np
 
