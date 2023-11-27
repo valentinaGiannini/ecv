b919c90194d3ba6d9ceae678fe31be1c624ad281
Valentina Giannini <47178936+valentinaGiannini@users.noreply.github.com>
2023-11-27 12:44:16 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install -r requirements.txt
-import SimpleITK as sitk
+# pip install -r requirements.txt
+# import SimpleITK as sitk
 import numpy as np
 
 st.title('Calcoliamo ecv')
@@ -13,21 +13,21 @@ basale_path = 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/basale.nii'
 le_path = 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/LE.nii'
 
 
-# Read the .nii image c
-basale = sitk.ReadImage(basale_path)
-lee = sitk.ReadImage(le_path)
-ecv_array = 0.6 * (sitk.GetArrayFromImage(lee) - sitk.GetArrayFromImage(basale))/198.0
-ecv_array[ecv_array < 0.0] = 0.0
-ecv_array[ecv_array > 8.0] = 8.0
-ecv_array_int=np.round(ecv_array*10)
+# # Read the .nii image c
+# basale = sitk.ReadImage(basale_path)
+# lee = sitk.ReadImage(le_path)
+# ecv_array = 0.6 * (sitk.GetArrayFromImage(lee) - sitk.GetArrayFromImage(basale))/198.0
+# ecv_array[ecv_array < 0.0] = 0.0
+# ecv_array[ecv_array > 8.0] = 8.0
+# ecv_array_int=np.round(ecv_array*10)
 
-ecv=sitk.GetImageFromArray(ecv_array_int)
-castFilter = sitk.CastImageFilter()
-castFilter.SetOutputPixelType(sitk.sitkInt16)
+# ecv=sitk.GetImageFromArray(ecv_array_int)
+# castFilter = sitk.CastImageFilter()
+# castFilter.SetOutputPixelType(sitk.sitkInt16)
 
-# Convert floating type image (imgSmooth) to int type (imgFiltered)
-imgFiltered = castFilter.Execute(ecv)
+# # Convert floating type image (imgSmooth) to int type (imgFiltered)
+# imgFiltered = castFilter.Execute(ecv)
 
-for i in range(imgFiltered.GetDepth()):
-    image_slice = imgFiltered[:, :, i]
-    sitk.WriteImage(image_slice, 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/dcm80/'+str(i)+'.dcm')
+# for i in range(imgFiltered.GetDepth()):
+#     image_slice = imgFiltered[:, :, i]
+#     sitk.WriteImage(image_slice, 'C:Users/Valentina/Downloads/COLLORAFI_LIDIA/dcm80/'+str(i)+'.dcm')
